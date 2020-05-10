82722303489b41f4319668baaad1257155d2a5ac
Arun et <arunet008@gmail.com>
2020-05-10 19:30:27 +0530

    10/May

--- public/js/treelo.js
+++ public/js/treelo.js
@@ -24,6 +24,23 @@ $(".sound").on("click", function() {
 	$("#soundGame").toggleClass("hide");
 })
 
+const user = document.getElementById("item");
+
+function catchItem(e) {
+  this.style.left = e.pageX - this.clientWidth / 2 + "px";
+  this.style.top = e.pageY - this.clientHeight / 2 + "px";
+  this.onmousemove = function(e) {
+  this.style.left = e.pageX - this.clientWidth / 2 + "px";
+  this.style.top = e.pageY - this.clientHeight / 2 + "px";
+  }
+  this.onmouseup = function() {
+  this.onmousemove = null; // onmouseup event [ redirect mousemove event signal to null instead of the drag-able element]
+  }
+}
+user.onmousedown = catchItem;
+
+
+
 window.onload = function() {
 
   setTimeout(() => {
